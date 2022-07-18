52bc971ca2c838d21bc7980525983b5072e95638
januszmiller <janusz.miller@hotmail.com>
2022-07-18 19:46:42 +0200

    Fix build on windows

--- project_build.bat
+++ project_build.bat
@@ -1,4 +1,1 @@
-docker run --rm -it \
- -v /src:/project \
- pico-builder \
- bash -c 'mkdir -p build && cd build && cmake .. && make blink'
+docker run --rm -it -v %cd%\src:/project pico-builder bash -c "mkdir -p build && cd build && cmake .. && make blink"
