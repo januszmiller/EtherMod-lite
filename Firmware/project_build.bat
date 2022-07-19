edf950909c8736a892195057fb8be274cca2db4d
januszmiller <janusz.miller@hotmail.com>
2022-07-19 17:13:04 +0200

    Add main.c and edit build command

--- Firmware/project_build.bat
+++ Firmware/project_build.bat
@@ -1,2 +1,2 @@
 rd /s /q %cd%\project\build
-docker run --rm -it -v %cd%\project:/project pico-builder bash -c "mkdir -p build && cd build && cmake .. && make blink"
+docker run --rm -it -v %cd%\project:/project pico-builder bash -c "mkdir -p build && cd build && cmake .. && make main"
