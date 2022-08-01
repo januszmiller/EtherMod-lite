ad7a293fc45fabe28e458e523a52c183314f0b3a
januszmiller <janusz.miller@hotmail.com>
2022-08-01 17:21:08 +0200

    Add PowerShell script for project building

---
+++ Firmware/build_project.ps1
@@ -0,0 +1,3 @@
+﻿if (-Not (docker images -q pico-builder)) {docker build -t pico-builder .}
+Remove-Item $pwd\project\build -Recurse
+docker run --rm -it -v $pwd\project:/project pico-builder bash -c "mkdir -p build && cd build && cmake .. && make main"
